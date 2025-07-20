7131c5d7a0a99618122c9732e45aa068703cea23
Ambrea27-tingz <ambreawilliams97@gmail.com>
2025-07-20 18:39:31 -0400

    P2 - Added back the same doc strings I had from before after deleting some of the code in this file due to some errors.

--- ship.py
+++ ship.py
@@ -6,6 +6,8 @@ if TYPE_CHECKING:
 
 class Ship(pygame.sprite.Sprite):
     def __init__(self, game: 'AlienInvasion', arsenal: 'Arsenal', *groups):
+        """Initialize the ship and set its starting position."""
+        # Call the parent class (Sprite) constructor from pygame
         super().__init__(*groups) 
         self.game = game
         self.settings = game.settings
@@ -25,14 +27,17 @@ class Ship(pygame.sprite.Sprite):
         self.arsenal = arsenal
 
     def _center_ship(self):
+        """Center the ship at the bottom of the screen."""
         self.rect.midbottom = self.boundaries.midbottom
         self.x = float(self.rect.x)
 
     def update(self):
+        """Update the ship's position based on movement flags."""
         self._update_ship_movement()
         self.arsenal.update_arsenal()
 
     def _update_ship_movement(self):
+        """Update the ship's position based on movement flags."""
         temp_speed = self.settings.ship_speed
         if self.moving_right and self.rect.right < self.boundaries.right:
             self.x += temp_speed
@@ -41,13 +46,16 @@ class Ship(pygame.sprite.Sprite):
         self.rect.x = int(self.x)  # prevents float assignment error
 
     def draw(self):
+        """Draw the ship and its arsenal to the screen."""
         self.arsenal.draw()
         self.screen.blit(self.image, self.rect)
 
     def fire(self):
+        """Fires a bullet if the arsenal limit is not reached."""
         return self.arsenal.fire_bullet()
 
     def check_collisions(self, other_group):
+        """Checks for collisions between the ship and another group."""
         if pygame.sprite.spritecollideany(self, other_group):  # type: ignore[arg-type] error
             self._center_ship()
             return True
