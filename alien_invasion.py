b41466719a272b4cf6d9eefca1f66bd5b57d2d27
Ambrea27-tingz <ambreawilliams97@gmail.com>
2025-07-24 13:59:32 -0400

    Made a small change for documentation.

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,15 +1,15 @@
 """ 
 Lab13_awilliams2_2.py / CSCI 1511
 
+Ambrea Williams
+
+Date: July 20,2025
+
 Title: Alien Invasion
 
 Discription: Alien Invasion is a creative game where the player controls a spaceship
 and tries to annihilate neigboring enemies by shooting down the invading aliens. 
 The game is built using Pygame.
-
-Author: Ambrea Williams
-
-Date: 2025-04-13
 """
 import sys
 import pygame
